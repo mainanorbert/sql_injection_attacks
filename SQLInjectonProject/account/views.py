6baf2edb8cbe4b1d02b8451c95a467e24cd8b7e5
mainanorbert <mainanorbert@gmail.com>
2024-07-05 19:07:26 +0300

    implemented navbar on the registration page

--- SQLInjectonProject/account/views.py
+++ SQLInjectonProject/account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.db import connection, IntegrityError
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login
 
 def register(request):
     """Register page for the app"""
@@ -35,5 +36,22 @@ def register(request):
 
 
 def login(request):
-    """login page"""
+    """Login page for the app"""
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        query = f"SELECT * FROM account_user WHERE username = '{username}' AND password = '{password}';"
+        
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            user = cursor.fetchone()
+        
+        if user:
+            messages.success(request, f"Welcome back, {username}!")
+            return redirect('home')  # Redirect to the home page or another page
+        else:
+            messages.error(request, "Invalid username or password!")
+            return redirect('login')
+    
     return render(request, 'login.html')
